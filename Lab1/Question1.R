6316397da9a229897759d639570aafd2e8ac37d4
Ansh-Ji <anshji666@gmail.com>
2020-09-05 08:51:13 +0530

    Completed Lab-04 on hypothesis testing (one categorical and one numerical variable)

--- Lab1/Question1.R
+++ Lab1/Question1.R
@@ -7,12 +7,13 @@ after<-c(53, 21 , 32, 49, 45, 38, 44, 33, 32, 43, 53, 46, 36, 48, 39, 35, 37, 36
 
 stem(before)
 stem(after)
+stem.leaf.backback(before,after)
 
 #(b) compute the five-point summaries and plot parallel boxplot
 summary(before)
 summary(after)
 boxplot(before,after,main = "Parallel Boxplots",
         xlab=("no of blocked intrusion attempts"),
-        names=c('Before','After'),col = c("orange","red"),
+        names=c('Before','Af'),col = c("orange","red"),
         border = "brown",horizontal = TRUE)
 
